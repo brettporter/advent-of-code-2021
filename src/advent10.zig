ab0a0c8d00c51ce86c4f6b8c328332706c29f46e
Brett Porter <brett.porter@safetyculture.io>
2021-12-11 15:26:44 +1100

    day 10, part 2

--- src/advent10.zig
+++ src/advent10.zig
@@ -10,13 +10,19 @@ const print = std.debug.print;
 
 // HELPERS
 
-const SCORE_OK: i32 = 0;
-const SCORE_RIGHT_PAREN: i32 = 3;
-const SCORE_RIGHT_BRACKET: i32 = 57;
-const SCORE_RIGHT_BRACE: i32 = 1197;
-const SCORE_GT: i32 = 25137;
+const ResultType = enum { corrupted, completed, ok };
+const Result = union(ResultType) {
+    corrupted: u64,
+    completed: u64,
+    ok: void,
+};
 
-fn checkChunkCorruption(allocator: Allocator, input: []const u8) !i32 {
+const SCORE_RIGHT_PAREN = 3;
+const SCORE_RIGHT_BRACKET = 57;
+const SCORE_RIGHT_BRACE = 1197;
+const SCORE_GT = 25137;
+
+fn checkChunkCorruption(allocator: Allocator, input: []const u8) !Result {
     const L = std.SinglyLinkedList(u8);
     var pairs = L{};
     defer {
@@ -35,35 +41,84 @@ fn checkChunkCorruption(allocator: Allocator, input: []const u8) !i32 {
             ')' => {
                 var n = pairs.popFirst().?;
                 defer allocator.destroy(n);
-                if (n.data != '(') return SCORE_RIGHT_PAREN;
+                if (n.data != '(') return Result{ .corrupted = SCORE_RIGHT_PAREN };
             },
             ']' => {
                 var n = pairs.popFirst().?;
                 defer allocator.destroy(n);
-                if (n.data != '[') return SCORE_RIGHT_BRACKET;
+                if (n.data != '[') return Result{ .corrupted = SCORE_RIGHT_BRACKET };
             },
             '}' => {
                 var n = pairs.popFirst().?;
                 defer allocator.destroy(n);
-                if (n.data != '{') return SCORE_RIGHT_BRACE;
+                if (n.data != '{') return Result{ .corrupted = SCORE_RIGHT_BRACE };
             },
             '>' => {
                 var n = pairs.popFirst().?;
                 defer allocator.destroy(n);
-                if (n.data != '<') return SCORE_GT;
+                if (n.data != '<') return Result{ .corrupted = SCORE_GT };
             },
             else => return error.InvalidData,
         }
     }
-    return SCORE_OK;
+
+    if (pairs.len() == 0) return Result{ .ok = undefined };
+
+    var completion = try ArrayList(u8).initCapacity(allocator, pairs.len());
+    defer completion.deinit();
+    while (pairs.popFirst()) |node| {
+        try completion.append(switch (node.data) {
+            '(' => ')',
+            '[' => ']',
+            '{' => '}',
+            '<' => '>',
+            else => unreachable(),
+        });
+        allocator.destroy(node);
+    }
+
+    return Result{ .completed = scoreCompletion(completion.items) };
 }
 
-fn calculateSyntaxError(allocator: Allocator, input: [][]const u8) !i32 {
-    var total: i32 = 0;
-    for (input) |l| {
-        total += try checkChunkCorruption(allocator, l);
+fn scoreCompletion(input: []const u8) u64 {
+    var total: u64 = 0;
+    for (input) |c| {
+        var score: u64 = switch (c) {
+            ')' => 1,
+            ']' => 2,
+            '}' => 3,
+            '>' => 4,
+            else => unreachable(),
+        };
+        total = total * 5 + score;
     }
     return total;
+}
+
+fn findWinner(scores: []u64) u64 {
+    std.sort.sort(u64, scores, {}, comptime std.sort.desc(u64));
+    return scores[(scores.len - 1) / 2];
+}
+
+fn calculateSyntaxError(allocator: Allocator, input: [][]const u8) !u64 {
+    var total: u64 = 0;
+    for (input) |l| {
+        var result = try checkChunkCorruption(allocator, l);
+        if (result == ResultType.corrupted) total += result.corrupted;
+        // ignore others
+    }
+    return total;
+}
+
+fn calculateCompletionScore(allocator: Allocator, input: [][]const u8) !u64 {
+    var scores = ArrayList(u64).init(allocator);
+    defer scores.deinit();
+    for (input) |l| {
+        var result = try checkChunkCorruption(allocator, l);
+        if (result == ResultType.completed) try scores.append(result.completed);
+        // ignore others
+    }
+    return findWinner(scores.items);
 }
 
 // MAIN
@@ -75,6 +130,7 @@ pub fn main() anyerror!void {
 
     var input = try common.readFile(allocator, "data/day10input.txt");
     print("Day 10: syntax error = {d}\n", .{calculateSyntaxError(allocator, input)});
+    print("Day 10: completion score = {d}\n", .{calculateCompletionScore(allocator, input)});
 }
 
 // TESTING
@@ -100,19 +156,33 @@ test "Example" {
     }
 
     try expect((try calculateSyntaxError(test_allocator, input)) == 26397);
+    try expect((try calculateCompletionScore(test_allocator, input)) == 288957);
 }
 
 test "Check corrupt chunks" {
-    try expect((try checkChunkCorruption(test_allocator, "()")) == 0);
-    try expect((try checkChunkCorruption(test_allocator, "[]")) == 0);
-    try expect((try checkChunkCorruption(test_allocator, "([])")) == 0);
-    try expect((try checkChunkCorruption(test_allocator, "{()()()}")) == 0);
-    try expect((try checkChunkCorruption(test_allocator, "<([{}])>")) == 0);
-    try expect((try checkChunkCorruption(test_allocator, "[<>({}){}[([])<>]]")) == 0);
-    try expect((try checkChunkCorruption(test_allocator, "(((((((((())))))))))")) == 0);
+    try expect((try checkChunkCorruption(test_allocator, "()")) == ResultType.ok);
+    try expect((try checkChunkCorruption(test_allocator, "[]")) == ResultType.ok);
+    try expect((try checkChunkCorruption(test_allocator, "([])")) == ResultType.ok);
+    try expect((try checkChunkCorruption(test_allocator, "{()()()}")) == ResultType.ok);
+    try expect((try checkChunkCorruption(test_allocator, "<([{}])>")) == ResultType.ok);
+    try expect((try checkChunkCorruption(test_allocator, "[<>({}){}[([])<>]]")) == ResultType.ok);
+    try expect((try checkChunkCorruption(test_allocator, "(((((((((())))))))))")) == ResultType.ok);
 
-    try expect((try checkChunkCorruption(test_allocator, "(]")) == SCORE_RIGHT_BRACKET);
-    try expect((try checkChunkCorruption(test_allocator, "{()()()>")) == SCORE_GT);
-    try expect((try checkChunkCorruption(test_allocator, "(((()))}")) == SCORE_RIGHT_BRACE);
-    try expect((try checkChunkCorruption(test_allocator, "<([]){()}[{}])")) == SCORE_RIGHT_PAREN);
+    try expect((try checkChunkCorruption(test_allocator, "(]")).corrupted == SCORE_RIGHT_BRACKET);
+    try expect((try checkChunkCorruption(test_allocator, "{()()()>")).corrupted == SCORE_GT);
+    try expect((try checkChunkCorruption(test_allocator, "(((()))}")).corrupted == SCORE_RIGHT_BRACE);
+    try expect((try checkChunkCorruption(test_allocator, "<([]){()}[{}])")).corrupted == SCORE_RIGHT_PAREN);
+}
+
+test "Check completion scoring" {
+    try expect(scoreCompletion("}}]])})]") == 288957);
+    try expect(scoreCompletion(")}>]})") == 5566);
+    try expect(scoreCompletion("}}>}>))))") == 1480781);
+    try expect(scoreCompletion("]]}}]}]}>") == 995444);
+    try expect(scoreCompletion("])}>") == 294);
+}
+
+test "Check winner" {
+    var scores = [_]u64{ 288957, 5566, 1480781, 995444, 294 };
+    try expect(findWinner(scores[0..]) == 288957);
 }
